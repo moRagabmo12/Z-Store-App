1431df2dbfb4ec7886f71705190af6fc8a4f9308
moRagabmo12 <moragabmohamed12@gmail.com>
2025-12-03 14:28:04 +0200

    change logo

--- lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash_view/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/assets.dart';
+import 'package:z_store_app/core/utils/assets.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Image.asset(AssetsData.test)]);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      children: [
+        Image.asset(AssetsData.logo),
+        Text(
+          "Z",
+          style: TextStyle(
+            color: Color(0xffE71B21),
+            fontSize: 100,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
   }
 }
